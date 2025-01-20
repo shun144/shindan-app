864d5ace77bb481210712f030674f26d5d6bce4a
shun144 <noelgld13@gmail.com>
2025-01-21 08:40:20 +0900

    Firebase設定情報の.env移行を切り戻し

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -4,13 +4,22 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC0oIWwcNpp4r5wmGV1mh0E87RQq0fE7xs",
+  authDomain: "shindan-app.firebaseapp.com",
+  projectId: "shindan-app",
+  storageBucket: "shindan-app.firebasestorage.app",
+  messagingSenderId: "81307988326",
+  appId: "1:81307988326:web:e3d01cb11f699a1b47d8a7",
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
