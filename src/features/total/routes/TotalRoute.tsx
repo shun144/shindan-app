7466a2cc0bbf58b8b3fd593e3da979a784474b54
shun144 <noelgld13@gmail.com>
2025-01-24 18:07:44 +0900

    table背景色変更

--- src/features/total/routes/TotalRoute.tsx
+++ src/features/total/routes/TotalRoute.tsx
@@ -8,8 +8,8 @@ const TotalRoute = () => {
   const { total } = useLoaderData<TotalLoaderData>();
 
   return (
-    <div className="py-6 w-full max-w-screen-lg bg-red-200">
-      <Suspense fallback={<div className="text-red-400">Loading Comments...</div>}>
+    <div className="py-6 w-full max-w-screen-lg">
+      <Suspense fallback={<div className="text-red-50">Loading Comments...</div>}>
         <Await resolve={total} errorElement={<ErrorPage />}>
           {(totalTableData) => <TotalTable totalTableData={totalTableData} />}
         </Await>
